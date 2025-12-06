9a448004e7c9a9299f2c12e56960937bcb7cf262
2IA08_Steven Kurniawan <kurniawansteven429@gmail.com>
2025-12-06 06:08:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6952677991-fccb0",
+  "appId": "1:655788035286:web:bb4ac87e14fee4b0040d77",
+  "apiKey": "AIzaSyD_tVDr4RHxsGBqe26neyf0JKQdD5QLh_E",
+  "authDomain": "studio-6952677991-fccb0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "655788035286"
+};
