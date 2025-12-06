adc273981bc227ce94f59099aa3e69dac413cc61
2IA08_Steven Kurniawan <kurniawansteven429@gmail.com>
2025-12-06 06:51:51 +0000

    Console Error Server
    You specified knownHelpersOnly, but used the unkn

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { z } from "zod";
+import Handlebars from "handlebars";
 import {
   generateAuthorBioOptions,
   GenerateAuthorBioOptionsInput,
@@ -20,6 +21,16 @@ import { initializeFirebase } from "@/firebase";
 import { addDocumentNonBlocking } from "@/firebase/non-blocking-updates";
 import type { GenerateThemeOutput } from "@/lib/types";
 import { author, projects } from "@/lib/data";
+
+// Register Handlebars helpers globally for all prompts
+Handlebars.registerHelper('json', function(context) {
+    return JSON.stringify(context, null, 2);
+});
+
+Handlebars.registerHelper('eq', function(a, b) {
+    return a === b;
+});
+
 
 export async function handleGenerateBio(
   input: GenerateAuthorBioOptionsInput
